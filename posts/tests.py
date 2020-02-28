a6bda9606f8187c7430241e91885c6c66221c6ac
athea0001 <alexandre.thean@gmail.com>
2020-02-28 14:59:22 +0900

    Rename post to posts, and add some more tests

--- posts/tests.py
+++ posts/tests.py
@@ -1,21 +1,34 @@
 from django.test import TestCase
+from django.urls import reverse
 
 from .models import Post
 
-
 # Create your tests here.
-class PostModelTest(TestCase):
 
+test_string = 'test'
+test_string_with_special_character = 'tést^'
+
+def before():
+    Post.objects.create(text=test_string)
+    Post.objects.create(text=test_string_with_special_character)
+
+class PostPageViewTest(TestCase):
     def test_post_page(self):
+        before()
         response = self.client.get('/posts/')
         self.assertEqual(response.status_code, 200)
 
+    def test_post_page_by_name(self):
+        response = self.client.get(reverse('posts'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_post_page_correct_template(self):
+        response = self.client.get(reverse('posts'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'post.html')
+
+class PostModelTest(TestCase):
     def test_text_content(self):
-        test_string = 'test'
-        test_string_with_special_character = 'tést^'
-
-        Post.objects.create(text=test_string)
-        Post.objects.create(text=test_string_with_special_character)
-
+        before()
         self.assertEqual(f'{Post.objects.get(id=1).text}', test_string)
         self.assertEqual(f'{Post.objects.get(id=2).text}', test_string_with_special_character)
--- posts/urls.py
+++ posts/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from .views import PostPageView
 
 urlpatterns = [
-    path('posts/', PostPageView.as_view(), name = "post")
+    path('posts/', PostPageView.as_view(), name = "posts")
 ]
