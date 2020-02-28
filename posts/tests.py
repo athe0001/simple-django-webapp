e3e9a9b44b76d7ce3887a45ab08249f9d898eaad
athea0001 <alexandre.thean@gmail.com>
2020-02-28 12:16:06 +0900

    Add tests to the posts page (checking model content and status code)

--- posts/tests.py
+++ posts/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
 
+from .models import Post
+
+
 # Create your tests here.
+class PostModelTest(TestCase):
+
+    def test_post_page(self):
+        response = self.client.get('/posts/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_text_content(self):
+        test_string = 'test'
+        test_string_with_special_character = 'tést^'
+
+        Post.objects.create(text=test_string)
+        Post.objects.create(text=test_string_with_special_character)
+
+        self.assertEqual(f'{Post.objects.get(id=1).text}', test_string)
+        self.assertEqual(f'{Post.objects.get(id=2).text}', test_string_with_special_character)
