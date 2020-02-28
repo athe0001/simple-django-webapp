1f2f51f1a290dcfa02ad72c48fc0afbde031ad5a
athea0001 <alexandre.thean@gmail.com>
2020-02-28 11:36:17 +0900

    Add tests to the 2 created pages to check the http status 200

--- pages/tests.py
+++ pages/tests.py
@@ -1,3 +1,12 @@
-from django.test import TestCase
+from django.test import SimpleTestCase
 
 # Create your tests here.
+
+class PagesTests(SimpleTestCase):
+    def test_hello_world_page(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_about_page(self):
+        response = self.client.get('/about/')
+        self.assertEqual(response.status_code, 200)
